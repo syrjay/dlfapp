3ac189908e6a9a98c5e51f01c743a442a32a63a2
syrjay <sy.jroumar@gmail.com>
2022-04-05 12:58:25 +0000

    deleting my firebase credentials

--- firebase.js
+++ firebase.js
@@ -7,12 +7,7 @@ import {
 import { getStorage } from "firebase/storage";
 import { initializeFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDFBLleE0Vq8-oArkKlbeVphvVOJw6ZVd0",
-  authDomain: "dic3chatapp.firebaseapp.com",
-  projectId: "dic3chatapp",
-  storageBucket: "dic3chatapp.appspot.com",
-  messagingSenderId: "996224019376",
-  appId: "1:996224019376:web:c8d5c85d1bc0c95f83c475"
+  // your firebase credentials here
 };
 
 export const app = initializeApp(firebaseConfig);
